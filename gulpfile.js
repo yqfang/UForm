3c7baecb15f1580f026edabfee7ba40efe9c5638
yqfang <404762352@qq.com>
2016-07-03 10:58:42 +0800

    ok

--- gulpfile.js
+++ gulpfile.js
@@ -44,13 +44,14 @@ gulp.task('scripts', ['clean'], function () {
             }))
             .pipe($.ngAnnotate())
             .pipe($.concat('uform_without_templates.js'))
-            .pipe($.header('(function() { \n"user strict";\n'))
+            .pipe($.header('(function() { \n"use strict";\n'))
             .pipe($.footer('\n}());'))
     };
     return es.merge(buildLib(), buildTemplates())
         .pipe($.plumber({
              errorHandler: handleError
         }))
+        .pipe($.concat('uform.js'))
         .pipe($.header(config.banner, {
             timestamp: (new Date()).toISOString(), pkg: config.pkg
         }))
@@ -65,67 +66,3 @@ var handleError = function (err) {
   console.log(err.toString());
   this.emit('end');
 };
-
-
-// var gulp = require("gulp");
-// var ngHtml2Js = require("gulp-ng-html2js");
-// var minifyHtml = require("gulp-minify-html");
-// var concat = require("gulp-concat");
-// var uglify = require("gulp-uglify");
-// var through = require('through2');
-// var ngAnnotate = require('gulp-ng-annotate');
-// gulp.task("compile-html-2-js",function(){
-// 	gulp.src(["./form-templates/**.html","./field-templates/**/**.html"])
-// 	// .pipe(minifyHtml({
-// 	// 	empty: true,
-// 	// 	spare: true,
-// 	// 	quotes: true
-// 	// }))
-// 	.pipe(ngHtml2Js({
-// 		moduleName: "uForm",
-// 		prefix: "templates/",
-// 		rename: function(templateUrl, templateFile){
-// 			var arr = templateUrl.split("/");
-// 			return arr[0] + "/" + arr[arr.length - 1];
-// 		}
-// 	}))
-// 	.pipe(concat("templates.js"))
-// 	// .pipe(uglify())
-// 	.pipe(gulp.dest("./dist/templates"));
-// })
-// gulp.task("compile-html-2-js-min",function(){
-// 	gulp.src(["./form-templates/**.html","./field-templates/**/**.html"])
-// 	.pipe(minifyHtml({
-// 		empty: true,
-// 		spare: true,
-// 		quotes: true
-// 	}))
-// 	.pipe(ngHtml2Js({
-// 		moduleName: "uForm",
-// 		prefix: "templates/",
-// 		rename: function(templateUrl, templateFile){
-// 			var arr = templateUrl.split("/");
-// 			return arr[0] + "/" + arr[arr.length - 1];
-// 		}
-// 	}))
-// 	.pipe(concat("templates.min.js"))
-// 	.pipe(uglify())
-// 	.pipe(gulp.dest("./dist/templates"));
-// })
-// gulp.task('compile-js',function(){
-// 	gulp.src(["./upForm.js","./field-templates/ext/**/**.js","./dist/templates/templates.js"])
-// 	.pipe(concat('upForm.js'))
-// 	.pipe(ngAnnotate())
-// 	.pipe(gulp.dest('./dist'))
-
-// });
-// gulp.task('compile-js-min',function(){
-// 	gulp.src(["./upForm.js","./field-templates/ext/**/**.js","./dist/templates/templates.min.js"])
-// 	.pipe(concat('upForm.min.js'))
-// 	.pipe(ngAnnotate())
-// 	.pipe(uglify())
-// 	.pipe(gulp.dest('./dist'))
-
-// });
-// gulp.task("generate",["compile-html-2-js",'compile-js'])
-// gulp.task("generate-min",["compile-html-2-js-min",'compile-js-min'])
