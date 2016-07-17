4a87035bcbc9184d74ab9eb26be8e32e703f5db4
yqfang <404762352@qq.com>
2016-07-18 01:07:23 +0800

    remove validateOn

--- src/model/ufieldProvider.js
+++ src/model/ufieldProvider.js
@@ -1,12 +1,10 @@
 uf.provider('ufield', [function() {
     var _tp = 'up-text'; // type
-    var _vo = 'dirty'; // validateOn
     var _pt = /^.*$/; // defaut pattern
     var _setOpts = function(opts){
         var _opts = {};
         opts = opts || {};
         _opts.type = (angular.isDefined(opts.type)) ? opts.type : _tp; // type
-        _opts.validateOn = (angular.isDefined(opts.validateOn) && ((opts.validateOn === 'dirty') || (opts.validateOn === 'blur'))) ? opts.validateOn : _vo; // validate_on
         _opts.pattern = (angular.isDefined(opts.pattern)) ? opts.pattern : _pt;
         return _opts;
     }; // end _setOpts
